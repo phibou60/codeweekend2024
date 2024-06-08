2f38ace46f785cb7a92827c6ddcc4306c2a7f59b
phibou60 <philippe.bouvart.pro@gmail.com>
2024-06-08 09:49:27 +0200

    Ajout printGameInput

--- projet_codeweekend2024/src/main/java/commons/GameState.java
+++ projet_codeweekend2024/src/main/java/commons/GameState.java
@@ -2,6 +2,8 @@ package commons;
 
 import java.util.List;
 
+import commons.model.GameInput;
+
 /**
  * Classe des données en mémoire
  */
@@ -18,4 +20,45 @@ public class GameState {
         return gameState;
     }
 
+    /**
+     * Print the gameState
+     */
+    public static void printGameInput(GameInput input, int height) {
+        double ratio = (double) input.getWidth() / input.getHeight();
+        
+        int width = (int) Math.round(height * ratio);
+        double scale = (double) input.getHeight() / height;
+        
+        char[][] board = new char[height][width];
+        
+        int x = (int) Math.round(input.getStartX() / scale);
+        int y = (int) Math.round(input.getStartY() / scale);
+        
+        board[y][x] = 'X';
+        
+        char[] chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        
+        for (int i = 0; i < input.getMonsters().size(); i++) {
+            x = (int) Math.round(input.getMonsters().get(i).getX() / scale);
+            y = (int) Math.round(input.getMonsters().get(i).getY() / scale);
+            
+            board[y][x] = chars[i];
+        }
+        
+        String border = "";
+        for (x = 0; x < width; x++) border += "--";
+        border = "+" + border + "+";
+        System.out.println(border);
+        
+        for (y = 0; y < height; y++) {
+            String line = "";
+            for (x = 0; x < width; x++) {
+                line += board[y][x] + " ";
+            }
+            System.out.println("!" + line + "!");
+        }
+        
+        System.out.println(border);
+        
+    }
 }
